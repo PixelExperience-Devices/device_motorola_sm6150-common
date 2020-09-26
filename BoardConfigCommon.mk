051f433f20f867994358cbd7c3e0374877094dd4
Felix Elsner <google@ix5.org>
2020-09-26 11:23:25 +0200

    sm6150-common: Use SYSTEM_EXT_*_SEPOLICY_DIRS
    The old variable name is being deprecated on API level 31+.
    
    See https://android-review.googlesource.com/q/topic:"board-system-ext-sepolicy"
    
    Signed-off-by: Felix Elsner <google@ix5.org>
    Change-Id: Ief4d1e64b8c8df5d6d04722ae0cf5ae5ee805b72

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -228,7 +228,7 @@ VENDOR_SECURITY_PATCH := 2021-01-01
 # SELinux
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Android Verified Boot
