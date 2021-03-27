cdcf86107f6b539efb70ac13fda0b588b46c8eed
Erfan Abdi <erfangplus@gmail.com>
2021-03-27 21:48:10 +0430

    sm6150-common: Update gpt-utils header pinned partitions

--- gpt-utils/gpt-utils.h
+++ gpt-utils/gpt-utils.h
@@ -80,8 +80,8 @@ extern "C" {
 #define AB_SLOT_B_SUFFIX                "_b"
 #define PTN_XBL                         "xbl"
 #define PTN_XBL_CFG                     "xbl_config"
-#define PTN_SWAP_LIST                   PTN_XBL, PTN_XBL_CFG, "abl", "aop", "devcfg", "hyp", "keymaster", "prov", "qupfw", "storsec", "tz", "uefisecapp", "vbmeta", "vbmeta_system"
-#define AB_PTN_LIST PTN_SWAP_LIST, "bluetooth", "boot", "dsp", "dtbo", "fsg", "logo", "modem", "recovery"
+#define PTN_SWAP_LIST                   PTN_XBL, PTN_XBL_CFG, "abl", "aop", "cmnlib64", "cmnlib", "devcfg", "hyp", "keymaster", "multiimgoem", "multiimgqti", "prov", "qupfw", "storsec", "tz", "uefisecapp", "vbmeta"
+#define AB_PTN_LIST PTN_SWAP_LIST, "bluetooth", "boot", "dsp", "dtbo", "fsg", "logo", "modem", "oem", "recovery", "system", "vendor"
 #define BOOT_DEV_DIR    "/dev/block/bootdevice/by-name"
 
 /******************************************************************************
