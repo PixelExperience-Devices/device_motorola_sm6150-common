4d9878539dc6b59348ff0d968cdb6bdb8b3ea86a
Erfan Abdi <erfangplus@gmail.com>
2021-03-28 12:40:36 +0430

    sm6150-common: Drop duplicated trust hal package

--- common.mk
+++ common.mk
@@ -505,10 +505,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-qti
