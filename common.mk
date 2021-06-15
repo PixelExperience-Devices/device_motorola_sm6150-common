a7cbee6def789bad9e25c5765857560bffa13493
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-06-15 10:25:05 +0200

    sm6150-common: Snap - NO
    Change-Id: I887b7557e3c49c5332ff41fe8039cb8d70e829ac

--- common.mk
+++ common.mk
@@ -168,7 +168,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
