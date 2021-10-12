da045ef33dd5b02932cd23f1664a04ada8bc1217
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 09:34:08 +0200

    sm6150-common: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: Ia8cef3905c133441cbe741c83d6d5ecdf4be2338

--- common.mk
+++ common.mk
@@ -118,7 +118,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.sm6150 \
