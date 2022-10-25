81604c3d72c12c159a99e7f54ca1054ade6e3df1
Andrew Hexen <SyberHexen@gmail.com>
2022-10-25 16:05:49 -0300

    sm6150-common: Guard audio effects for liber
    Change-Id: Ic902fd6071bb959bba01861a8bf6943e36a7bdb1
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- common.mk
+++ common.mk
@@ -137,8 +137,12 @@ PRODUCT_PACKAGES += \
     libtinycompress \
     libtinycompress.vendor
 
+ifeq ($(filter %_liber,$(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+endif
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
